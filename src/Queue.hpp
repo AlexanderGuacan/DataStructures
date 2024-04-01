fb28e4c8a2f2af92bfffb38ce618ec6569447fb5
AlexanderGuacan <adguacan2003@gmail.com>
2024-04-01 11:24:35 -0500

    Add template argument in all instances

--- src/Queue.hpp
+++ src/Queue.hpp
@@ -15,20 +15,20 @@ class Queue {
     Node<T>* head{};
     Node<T>* tail{};
 
-    void copy(const Queue& queue);
+    void copy(const Queue<T>& queue);
 
     public:
 
     Queue() = default;
     Queue(std::initializer_list<T> list);
-    Queue(const Queue& queue);
+    Queue(const Queue<T>& queue);
     int getLength();
     const T& getHead() const;
     void enqueue(const T& element);
     T dequeue();
     bool isEmpty() const;
     void clear();
-    Queue& operator=(const Queue& rightHandSideOperator);
+    Queue<T>& operator=(const Queue<T>& rightHandSideOperator);
     template <typename _T>
     friend bool operator==(const Queue<_T>& leftHandSideOperator, const Queue<_T>& rightHandSideOperator);
     template <typename _T>
